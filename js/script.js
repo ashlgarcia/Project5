d12c26e2fef223cbb50990f4e3646e7930d5a062
ashlgarcia <142343742+ashlgarcia@users.noreply.github.com>
2023-11-04 11:13:31 -0700

    Update script.js
    Basic Project 9 adjustments

--- js/script.js
+++ js/script.js
@@ -1,25 +1,19 @@
 // Project 9 Additions
 
-// Add toggle switch and the body element
-const modeToggle = document.getElementById("modeToggle");
+const modeToggle = document.getElementById('mode-toggle-checkbox');
 const body = document.body;
+const nav = document.querySelector('nav');
 
-// Check for the user's preference in local storage
-const currentMode = localStorage.getItem("mode");
-if (currentMode === "dark") {
-    body.classList.add("dark-mode");
-    modeToggle.checked = true;
-}
-
-// Adding event listener to the toggle switch
-modeToggle.addEventListener("change", () => {
-    if (modeToggle.checked) {
-        body.classList.add("dark-mode");
-        localStorage.setItem("mode", "dark");
-    } else {
-        body.classList.remove("dark-mode");
-        localStorage.setItem("mode", "light");
-    }
+modeToggle.addEventListener('change', () => {
+  if (modeToggle.checked) {
+    // Switch to dark mode
+    body.classList.add('dark-mode');
+    nav.classList.add('dark-mode');
+  } else {
+    // Switch back to light mode
+    body.classList.remove('dark-mode');
+    nav.classList.remove('dark-mode');
+  }
 });
 
 // End of Project 9 Additions
