5afd18a34a5e46a2a46021edbc46f456fa4d8915
ashlgarcia <142343742+ashlgarcia@users.noreply.github.com>
2023-11-07 14:59:45 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
 // Project 9 Additions
 
-const modeToggle = document.getElementById('mode-toggle-checkbox');
-const body = document.body;
-const nav = document.querySelector('nav');
 
-modeToggle.addEventListener('change', () => {
-  if (modeToggle.checked) {
-    // Switch to dark mode
-    body.classList.add('dark-mode');
-    nav.classList.add('dark-mode');
-  } else {
-    // Switch back to light mode
-    body.classList.remove('dark-mode');
-    nav.classList.remove('dark-mode');
-  }
-});
 
 // End of Project 9 Additions
